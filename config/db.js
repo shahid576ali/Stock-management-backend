09d2b50b46e0a058b7915b17a69ad736817745ea
Subhash Kumar <subhashkushwah134@gmail.com>
2025-01-17 22:17:40 +0530

    added cloud DB URL

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/sales&stocks").then(()=>{
+    await mongoose.connect(`${process.env.DB_URL}sales&stocks`).then(()=>{
             console.log("Database connected");
         })
 };
