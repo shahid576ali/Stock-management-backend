3aeea5d1076538701d5df2330d3ac7c56d72ad66
Subhash Kumar <subhashkushwah134@gmail.com>
2025-01-18 17:38:19 +0530

    updated db connection and error handling

--- config/db.js
+++ config/db.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    await mongoose.connect(`${process.env.DB_URL}sales&stocks`).then(()=>{
-            console.log("Database connected");
-        })
+    try {
+        const conn = await mongoose.connect(`${process.env.DB_URL}sales&stocks`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`Database connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
 };
 
-export default connectDB
+export default connectDB;
